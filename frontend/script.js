c7f566b0fd74b92b7d737eb29d255d7a863242f5
Manan Kumawat <manan.kumawat.cse24@itbhu.ac.in>
2026-08-03 01:07:37 +0530

    Refactored edit/save, cancel buttons, and code

--- frontend/script.js
+++ frontend/script.js
@@ -1,28 +1,45 @@
 const addHeader = document.getElementById("addHeader");
+const addForm = document.getElementById("addForm");
+const searchForm = document.getElementById("searchForm");
+const crimeTable = document.getElementById("crimeTable");
+
+const API_URL = "http://127.0.0.1:8000";
+
+const addFields = [
+  { key: "Crime Id", id: "addCrimeId" },
+  { key: "Original Crime Type Name", id: "addCrimeType" },
+  { key: "Report Date", id: "addReportDate" },
+  { key: "Call Date", id: "addCallDate" },
+  { key: "Offense Date", id: "addOffenseDate" },
+  { key: "Call Time", id: "addCallTime" },
+  { key: "Disposition", id: "addDisposition" },
+  { key: "Address", id: "addAddress" },
+  { key: "City", id: "addCity" },
+  { key: "State", id: "addState" },
+  { key: "Address Type", id: "addAddressType" },
+  { key: "Case Status", id: "addStatus" },
+];
+
+const editableFields = [
+  { column: 2, key: "Original Crime Type Name", type: "text" },
+  { column: 3, key: "Report Date", type: "date" },
+  { column: 4, key: "Call Date", type: "date" },
+  { column: 5, key: "Offense Date", type: "date" },
+  { column: 6, key: "Call Time", type: "time" },
+  { column: 7, key: "Disposition", type: "text" },
+  { column: 8, key: "Address", type: "text" },
+  { column: 9, key: "City", type: "text" },
+  { column: 10, key: "State", type: "text" },
+  { column: 11, key: "Address Type", type: "text" },
+  { column: 12, key: "Case Status", type: "text" },
+];
 
 addHeader.addEventListener("click", () => {
   addForm.classList.toggle("hidden");
 });
 
-const addForm = document.getElementById("addForm");
-
 addForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  const addFields = [
-    { key: "Crime Id", id: "addCrimeId" },
-    { key: "Original Crime Type Name", id: "addCrimeType" },
-    { key: "Report Date", id: "addReportDate" },
-    { key: "Call Date", id: "addCallDate" },
-    { key: "Offense Date", id: "addOffenseDate" },
-    { key: "Call Time", id: "addCallTime" },
-    { key: "Disposition", id: "addDisposition" },
-    { key: "Address", id: "addAddress" },
-    { key: "City", id: "addCity" },
-    { key: "State", id: "addState" },
-    { key: "Address Type", id: "addAddressType" },
-    { key: "Case Status", id: "addStatus" },
-  ];
 
   const newCrime = {};
 
@@ -30,7 +47,7 @@ addForm.addEventListener("submit", async (event) => {
     newCrime[field.key] = document.getElementById(field.id).value;
   });
 
-  const response = await fetch("http://127.0.0.1:8000/crimes", {
+  const response = await fetch(`${API_URL}/crimes`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -49,8 +66,113 @@ addForm.addEventListener("submit", async (event) => {
   searchForm.requestSubmit();
 });
 
-const searchForm = document.getElementById("searchForm");
-const crimeTable = document.getElementById("crimeTable");
+const createRow = (crime, index) => {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${crime["Crime Id"]}</td>
+      <td>${crime["Original Crime Type Name"]}</td>
+      <td>${crime["Report Date"]}</td>
+      <td>${crime["Call Date"]}</td>
+      <td>${crime["Offense Date"]}</td>
+      <td>${crime["Call Time"]}</td>
+      <td>${crime["Disposition"]}</td>
+      <td>${crime["Address"]}</td>
+      <td>${crime["City"]}</td>
+      <td>${crime["State"]}</td>
+      <td>${crime["Address Type"]}</td>
+      <td>${crime["Case Status"]}</td>
+    `;
+
+  const actionCell = document.createElement("td");
+  actionCell.classList.add("action-cell");
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.className = "button";
+
+  const cancelButton = document.createElement("button");
+  cancelButton.textContent = "Cancel";
+  cancelButton.classList.add("cancel-button", "hidden");
+
+  let editing = false;
+  editButton.addEventListener("click", async () => {
+    if (!editing) {
+      editableFields.forEach((field) => {
+        row.children[field.column].innerHTML =
+          `<input class="edit-input" type="${field.type}" value="${crime[field.key]}">`;
+      });
+      editButton.textContent = "Save";
+      cancelButton.classList.remove("hidden");
+
+      editing = true;
+    } else {
+      const updatedCrime = {};
+
+      editableFields.forEach((field) => {
+        const input = row.children[field.column].querySelector("input");
+        const value = input.value;
+
+        if (value !== crime[field.key]) {
+          updatedCrime[field.key] = value;
+        }
+
+        crime[field.key] = value;
+        row.children[field.column].textContent = value;
+      });
+
+      const response = await fetch(`${API_URL}/crimes/${crime["Crime Id"]}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedCrime),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        alert(error.detail);
+        return;
+      }
+
+      editButton.textContent = "Edit";
+      cancelButton.classList.add("hidden");
+      editing = false;
+    }
+  });
+
+  cancelButton.addEventListener("click", () => {
+    editableFields.forEach((field) => {
+      row.children[field.column].textContent = crime[field.key];
+    });
+    editButton.textContent = "Edit";
+    cancelButton.classList.add("hidden");
+    editing = false;
+  });
+
+  actionCell.appendChild(cancelButton);
+  actionCell.appendChild(editButton);
+  row.appendChild(actionCell);
+
+  return row;
+};
+
+const dummyCrime = {
+  "Crime Id": 190090430,
+  "Original Crime Type Name": "Vandalism",
+  "Report Date": "2019-01-09",
+  "Call Date": "2019-01-09",
+  "Offense Date": "2019-01-09",
+  "Call Time": "06:12:00",
+  Disposition: "ADV",
+  Address: "700 Block Of Taylor St",
+  City: "San Francisco",
+  State: "CA",
+  "Address Type": "Premise Address",
+  "Case Status": "Withdrawn",
+};
+
+crimeTable.appendChild(createRow(dummyCrime, 0));
 
 searchForm.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -73,117 +195,13 @@ searchForm.addEventListener("submit", async (event) => {
 
   if (status) params.append("status", status);
 
-  const url = "http://127.0.0.1:8000/crimes?" + params.toString();
+  const url = `${API_URL}/crimes?` + params.toString();
 
   const response = await fetch(url);
   const crimes = await response.json();
 
   crimeTable.innerHTML = "";
   crimes.forEach((crime, index) => {
-    const row = document.createElement("tr");
-
-    row.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${crime["Crime Id"]}</td>
-      <td>${crime["Original Crime Type Name"]}</td>
-      <td>${crime["Report Date"]}</td>
-      <td>${crime["Call Date"]}</td>
-      <td>${crime["Offense Date"]}</td>
-      <td>${crime["Call Time"]}</td>
-      <td>${crime["Disposition"]}</td>
-      <td>${crime["Address"]}</td>
-      <td>${crime["City"]}</td>
-      <td>${crime["State"]}</td>
-      <td>${crime["Address Type"]}</td>
-      <td>${crime["Case Status"]}</td>
-    `;
-
-    const actionCell = document.createElement("td");
-    actionCell.classList.add("action-cell");
-    const editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.className = "button";
-
-    const cancelButton = document.createElement("button");
-    cancelButton.textContent = "Cancel";
-    cancelButton.classList.add("cancel-button", "hidden");
-
-    const editableFields = [
-      { column: 2, key: "Original Crime Type Name", type: "text" },
-      { column: 3, key: "Report Date", type: "date" },
-      { column: 4, key: "Call Date", type: "date" },
-      { column: 5, key: "Offense Date", type: "date" },
-      { column: 6, key: "Call Time", type: "time" },
-      { column: 7, key: "Disposition", type: "text" },
-      { column: 8, key: "Address", type: "text" },
-      { column: 9, key: "City", type: "text" },
-      { column: 10, key: "State", type: "text" },
-      { column: 11, key: "Address Type", type: "text" },
-      { column: 12, key: "Case Status", type: "text" },
-    ];
-
-    let editing = false;
-    editButton.addEventListener("click", async () => {
-      if (!editing) {
-        editableFields.forEach((field) => {
-          row.children[field.column].innerHTML =
-            `<input type="${field.type}" value="${crime[field.key]}">`;
-        });
-        editButton.textContent = "Save";
-        cancelButton.classList.remove("hidden");
-
-        editing = true;
-      } else {
-        const updatedCrime = {};
-
-        editableFields.forEach((field) => {
-          const input = row.children[field.column].querySelector("input");
-          const value = input.value;
-
-          if (value !== crime[field.key]) {
-            updatedCrime[field.key] = value;
-          }
-
-          crime[field.key] = value;
-          row.children[field.column].textContent = value;
-        });
-
-        const response = await fetch(
-          `http://127.0.0.1:8000/crimes/${crime["Crime Id"]}`,
-          {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updatedCrime),
-          },
-        );
-
-        if (!response.ok) {
-          const error = await response.json();
-          alert(error.detail);
-          return;
-        }
-
-        editButton.textContent = "Edit";
-        cancelButton.classList.add("hidden");
-        editing = false;
-      }
-    });
-
-    cancelButton.addEventListener("click", () => {
-      editableFields.forEach((field) => {
-        row.children[field.column].textContent = crime[field.key];
-      });
-      editButton.textContent = "Edit";
-      cancelButton.classList.add("hidden");
-      editing = false;
-    });
-
-    actionCell.appendChild(cancelButton);
-    actionCell.appendChild(editButton);
-    row.appendChild(actionCell);
-
-    crimeTable.appendChild(row);
+    crimeTable.appendChild(createRow(crime, index));
   });
 });
